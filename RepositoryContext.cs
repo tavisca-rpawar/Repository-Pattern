6abf933ce3bbc7f5e671dafd8aac7f71bed4fa10
tavisca-rpawar <51902720+tavisca-rpawar@users.noreply.github.com>
2019-10-09 17:07:49 +0530

    Added files
    Initial commit

---
+++ RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) :base(options)
+        { }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Contracts;
+using Repository;
+namespace Entities
+{
+    public static class ServiceExtensions {
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("dev");
+            services.AddDbContext<RepositoryContext>(o => o.UseSqlServer(connectionString));
+        }
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+        }
+    }
+}
